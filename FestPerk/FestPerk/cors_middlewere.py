d1c6d164154ea916bb996f985c81df3a8c40855e
Gerard Gispert Carreras <gerardgispertcarreras@gmail.com>
2024-05-05 01:17:25 +0200

    head

--- FestPerk/FestPerk/cors_middlewere.py
+++ FestPerk/FestPerk/cors_middlewere.py
@@ -8,6 +8,6 @@ class DisableCorsMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
         response['Access-Control-Allow-Origin'] = '*'
-        response['Access-Control-Allow-Methods'] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
+        response['Access-Control-Allow-Methods'] = "DELETE, GET, OPTIONS, PATCH, POST, PUT, HEAD"
         response['Access-Control-Allow-Headers'] = "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with"
         return response
