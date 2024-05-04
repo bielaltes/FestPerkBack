e16f4026e56f06a472804702d9ba7f9eef959241
bielaltes <bielaltesgrifoll@gmail.com>
2024-05-04 19:06:55 +0200

    cors middleware

--- FestPerk/FestPerk/settings.py
+++ FestPerk/FestPerk/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'FestPerk.cors_middlewere.DisableCorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
