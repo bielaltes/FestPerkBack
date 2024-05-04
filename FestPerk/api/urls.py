a5d493ba12b87834c046bfddbe4ab3e1f06b5fc5
Gerard Gispert Carreras <gerardgispertcarreras@gmail.com>
2024-05-04 03:41:14 +0200

    endpoints: added base endpoints by rest_framework for every model

--- FestPerk/api/urls.py
+++ FestPerk/api/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path, include
 from rest_framework import routers
-from . import views
+from .views import (
+    TravelsViewSet,
+    CityViewSet,
+    LocalViewSet,
+    PartyViewSet,
+    ParticipantViewSet,
+)
 
 router = routers.DefaultRouter()
-# router.register(r'example', views.ExampleViewSet)
+router.register(r"travels", TravelsViewSet)
+router.register(r"cities", CityViewSet)
+router.register(r"locals", LocalViewSet)
+router.register(r"parties", PartyViewSet)
+router.register(r"participants", ParticipantViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
